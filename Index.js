5ecd3038e775a622ac87bc5d07ac46eb48383d40
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-03-16 16:16:59 +0100

    Done

--- Index.js
+++ Index.js
@@ -1,36 +1,65 @@
 const Express = require('express')
 const App = Express()
 const FS = require("fs")
-const Execute = require("child_process").execSync
 const PackageNameRegex = require('package-name-regex')
+const Fetch = require("sync-fetch")
+const SemVer = require("semver")
+
+function GetVersions(Package) {
+    return Fetch(
+        `https://registry.npmjs.org/${Package}`,
+        {
+            headers: {
+                Accept: "application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*"
+            }
+        }
+    ).json().versions
+}
 
 App.get(
-    "/versions/:package",
+    "/versions/",
     async function (Request, Response) {
-        console.log(Request.params.package)
+        console.log(Request.query.q)
 
-        const Package = Request.params.package
+        const Package = Request.query.q
         if (!PackageNameRegex.test(Package)) {
             Response.send(false)
             return
         }
 
-        var Output
-        try {
-            Output = Execute(`npm view ${Package} versions name --json`)
-        } catch (error) {
-            return Response.send(false)
+        const Output = {
+            Name: Package,
+            Versions: Object.keys(GetVersions(Package))
         }
-        const OutputData = JSON.parse(Output.toString())
 
-        if (typeof OutputData != "object") {
+        Response.send(Output)
+    }
+)
+
+App.get(
+    "/version/",
+    async function (Request, Response) {
+        console.log(Request.query.q)
+
+        const Package = Request.query.q
+        const Version = Request.query.v
+        if (!PackageNameRegex.test(Package)) {
             Response.send(false)
             return
         }
 
-        Response.send(OutputData)
+        const Versions = GetVersions(Package)
+        const MatchingVersion = SemVer.maxSatisfying(Object.keys(Versions), Version)
+        const Output = {
+            Name: Package,
+            Version: MatchingVersion,
+            VersionData: Versions[MatchingVersion]
+        }
+
+        Response.send(Output)
     }
 )
+
 
 const Port = FS.readFileSync(`${process.cwd()}/PortNumber`, "utf-8")
 console.log(`Listening at port number ${Port}`)
